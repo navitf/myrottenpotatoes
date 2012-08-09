31486581c6d03fb9a7db70cb9fbfa9f45d4f6cfe
navitf <navit.fedida@gmail.com>
2012-08-09 08:59:28 +0200

    add filter form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+	@all_ratings = Movie.all_ratings;
 	if (params[:sort_by]=='title')
 		@movies = Movie.find(:all, :order => "title")
 	elsif (params[:sort_by]=='release_date')
